918d619f13eb4ea403497718d7d6bf86ca1bc0e7
ZX_Gong <1045418215@qq.com>
2019-11-27 13:19:37 +0800

    check tiny mistakes

--- coverstory/models.py
+++ coverstory/models.py
@@ -32,7 +32,7 @@ class Player(BasePlayer):
     # check.
     check_q1 = models.BooleanField(label='1.参与者第5回合仍可以选择是否投币?', choices=[[1, '是'], [0, '否']], widget=widgets.RadioSelectHorizontal)
     check_q2 = models.BooleanField(label='2.032号参与者第4回合的实际收益是否为+2.5 ?', choices=[[1, '是'], [0, '否']], widget=widgets.RadioSelectHorizontal)
-    check_q3 = models.BooleanField(label='3.034号参与者第1和2回合是否选择了投币?', choices=[[1, '是'], [0, '否']], widget=widgets.RadioSelectHorizontal)
+    check_q3 = models.BooleanField(label='3.034号参与者第3和4回合是否选择了不投币?', choices=[[1, '是'], [0, '否']], widget=widgets.RadioSelectHorizontal)
 
 
 
